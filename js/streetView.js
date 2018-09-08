a9a34206ce4eebf47cca463239e38d52e0a4f171
Roy Crivolotti <roycrivolotti@gmail.com>
2018-09-08 11:04:12 -0300

    Street View setup

--- js/streetView.js
+++ js/streetView.js
@@ -2,11 +2,19 @@ streetViewModule = (function () {
   var paronama; // StreetView
 
   function init() {
-    /* TODO: creates a pano in a set position and displays it */
+    let centerPos = map.getCenter();
+    panorama = new google.maps.StreetViewPanorama(document.getElementById('streetView'), {
+      position: centerPos,
+      pov: {
+        heading: 34,
+        pitch: 10
+      }
+    });
+    map.setStreetView(panorama);
   }
 
-  function setStreetView(ubicacion) {
-    /* TODO: updates the pano variable position and changes the map to display the current position's StreetView */
+  function setStreetView(centerPos) {
+    panorama.setPosition(centerPos);
   }
 
   return {
